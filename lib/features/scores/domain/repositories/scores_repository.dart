4d8533b24a9ff5da7dd73021c59557e95cb8e5d3
emeban44 <podbicanin.emerald@gmail.com>
2025-11-24 18:33:49 +0100

    repository

---
+++ lib/features/scores/domain/repositories/scores_repository.dart
@@ -0,0 +1,32 @@
+import 'package:dartz/dartz.dart';
+import 'package:rolla_fitness_app_demo/core/error/failures.dart';
+import 'package:rolla_fitness_app_demo/features/scores/domain/entities/score.dart';
+import 'package:rolla_fitness_app_demo/features/scores/domain/entities/score_type.dart';
+import 'package:rolla_fitness_app_demo/features/scores/domain/entities/timeframe.dart';
+import 'package:rolla_fitness_app_demo/features/scores/domain/entities/score_history_point.dart';
+import 'package:rolla_fitness_app_demo/features/scores/domain/entities/insight.dart';
+import 'package:rolla_fitness_app_demo/features/scores/domain/entities/metric_info.dart';
+
+/// Repository interface for scores data
+abstract class ScoresRepository {
+  /// Get all scores for home screen
+  Future<Either<Failure, List<Score>>> getScores();
+
+  /// Get detailed score data for a specific type and timeframe
+  Future<Either<Failure, Score>> getScoreDetail(
+    ScoreType type,
+    Timeframe timeframe,
+  );
+
+  /// Get score history for charts
+  Future<Either<Failure, List<ScoreHistoryPoint>>> getScoreHistory(
+    ScoreType type,
+    Timeframe timeframe,
+  );
+
+  /// Get insights for a score type
+  Future<Either<Failure, List<Insight>>> getInsights(ScoreType type);
+
+  /// Get metric information for info drawer
+  Future<Either<Failure, MetricInfo>> getMetricInfo(String metricId);
+}
