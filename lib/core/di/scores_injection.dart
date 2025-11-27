b1cb74099f42cc768df3444efc2988f683ed2210
emeban44 <podbicanin.emerald@gmail.com>
2025-11-28 00:26:38 +0100

    removed comment

--- lib/core/di/scores_injection.dart
+++ lib/core/di/scores_injection.dart
@@ -6,8 +6,5 @@ import 'scores_injection.config.dart';
 /// Global service locator instance
 final getIt = GetIt.instance;
 
-/// Configures all dependencies using Injectable's code generation.
-/// Dependencies are registered automatically by scanning for @injectable and @lazySingleton
-/// annotations throughout the codebase. Run 'flutter pub run build_runner build' to generate.
 @InjectableInit()
 Future<void> configureDependencies() async => getIt.init();
